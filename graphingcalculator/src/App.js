b4a8543a3ab379e7a64d5da6024054b06cec9edd
brian <brianp01299@gmail.com>
2021-07-01 12:01:47 -0400

    Added graphing functionality

--- graphingcalculator/src/App.js
+++ graphingcalculator/src/App.js
@@ -2,30 +2,65 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react"
 import Component from "react"
+//import Plotly from "plotly"
 
 class App extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      "Derivative": 0,
+      "const1" : 0,
+      "minX": 0,
+      "minY":0,
+      "maxX":100,
+      "maxy":100,
+      "dataX":[],
+      "dataY":[]
+    }
+  }
+  produceDatePoints() {
+    this.state.dataX = []
+    this.state.dataY = []
+    for (var i =  this.state.minX;i<this.state.maxX;i++) {
+      this.state.dataX.push(i)
+      this.state.dataY.push(i)
+    }
+  }
+
+  graph() {
+    var graphDiv = document.getElementById('graph')
+    this.produceDatePoints()
+    var data = [{
+      x : this.state.dataX,
+      y : this.state.dataY,
+      type: 'scatter'
+    }];
+
+    var layout = {
+      title: 'Sales Growth',
+      xaxis: {
+        title: 'Year',
+        showgrid: false,
+        zeroline: false
+      },
+      yaxis: {
+        title: 'Percent',
+        showline: false
+      }
+    };
+    window.Plotly.newPlot(graphDiv, data, layout);
   }
   render() {
-
+    var that = this
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+         <input type ="text"/>
+         <button onClick = {function(){that.graph()}}> run </button>
+         <div id = "graph">
+
+         </div>
         </header>
       </div>
     );
